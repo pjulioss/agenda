d93a3860be061b742112e38e60a4a9e1da97577d
Pedro Julio <soaresjuliopedro@gmail.com>
2021-02-15 09:40:06 -0300

    Configuração inicial do projeto

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Agenda em PHP
 
-Um sistema de agenda em PHP para fins de estudos do PDO e conexão com MySQL.
+*Um sistema de agenda em PHP para fins de estudos do PDO e conexão com MySQL.*
+
+**Tecnologias usadas:**
+- PHP (PDO)
+- MySQL (PHP Admin)
+- Bootstrap
+- Font Awesome
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+//include_once "config/url.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+    <!-- Bootstrap / Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+        integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+        crossorigin="anonymous" />
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <h1>Teste</h1>
+    <i class="fas fa-eye"></i>
+</body>
+
+</html>
